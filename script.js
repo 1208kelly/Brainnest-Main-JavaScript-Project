59643ed94e52fcf9f346f331f3170b5f63448b26
Bob Kelso <49724332+1208kelly@users.noreply.github.com>
2023-02-02 20:29:37 +0000

    updating script.js file

--- script.js
+++ script.js
@@ -3,5 +3,22 @@ const choices = ["rock", "paper", "scissors"]
 function game(){
     const games = 5;  
     let playerPoints = 0; 
-    let computerPoints = 0; 
+    let computerPoints = 0;
+
+    for(let i = 0; i < games; i++) {
+        let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase(); 
+        function computerPlay() { return (["paper","scissors","rock"])[Math.random() * 3 | 0]; }
+        let computerSelection = computerPlay();
+
+        let round = playRound(playerSelection, computerSelection);
+        if(round === "wins"){
+        playerPoints++;
+        console.log("You Win");
+        } else if(round === "loses"){
+        computerPoints++;
+        console.log("You Lose");
+        } else if(round === "ties") {
+        console.log("tied");
+        } 
+    }
 }
